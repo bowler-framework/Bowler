1fce769951fae8c01548bdc8632f4bc239eccbf8
Wille Faler <wille.faler@gmail.com>
2011-01-26 00:08:34 +0000

    finished ComponentRenderSupport

--- core/src/main/scala/org/bowlerframework/view/scalate/ComponentRenderSupport.scala
+++ core/src/main/scala/org/bowlerframework/view/scalate/ComponentRenderSupport.scala
@@ -4,6 +4,7 @@ import org.fusesource.scalate.util.{Resource, FileResourceLoader}
 import java.io.{StringWriter, PrintWriter}
 import org.fusesource.scalate.{DefaultRenderContext, TemplateEngine}
 import org.bowlerframework.RequestScope
+import org.bowlerframework.model.ViewModelBuilder
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,7 +16,15 @@ import org.bowlerframework.RequestScope
 
 trait ComponentRenderSupport {
 
-  def render(model: Map[String, Any], action: String = "index"): String = {
+  def render(models: Any*): String = {
+    renderMap(ViewModelBuilder.buildModel(models.toSeq).toMap)
+  }
+
+  def renderWithNamed(action: String, models: Any*){
+    render(ViewModelBuilder.buildModel(models.toSeq), action)
+  }
+
+  def renderMap(model: Map[String, Any], action: String = "index"): String = {
     var uri = "/" + this.getClass.getName.replace(".", "/")
     uri = uri.replace("$", "") + "_" + action
     println(this.getClass.getName)
--- core/src/test/scala/org/bowlerframework/view/scalate/ComponentRenderSupportTest.scala
+++ core/src/test/scala/org/bowlerframework/view/scalate/ComponentRenderSupportTest.scala
@@ -1,6 +1,7 @@
 package org.bowlerframework.view.scalate
 
 import org.scalatest.FunSuite
+import java.io.{PrintWriter, StringWriter}
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,22 +18,35 @@ class ComponentRenderSupportTest extends FunSuite{
   }
 
   test("complext nexted renderable with model around"){
+    RenderEngine.reset
+    val uri =  "/org/bowlerframework/view/scalate/complex.ssp"
+    val model = Map("bean" -> Widget("hello", 50), "bean2" -> Widget("bye", 100), "tuple" -> Tuple2("foo", "bar"))
+    val writer = new StringWriter
+    val pw = new PrintWriter(writer)
+    val context = new BowlerRenderContext(uri, RenderEngine.getEngine, pw)
+
+    context.render(uri, model)
+    println(writer.toString)
+    assert(writer.toString == "hello 50 - A Foo is a bar bye 100")
 
   }
 
-  test("with multiple items in model"){
-
-  }
 
   test("with varargs (a la renderable)"){
-
+    assert("A Foo is a bar" == SimpleComponent.show(Tuple2("foo", "bar")))
   }
 }
 
 object SimpleComponent extends ComponentRenderSupport{
 
   def show: String = {
-    this.render(Map("foo" -> "bar"))
+    this.renderMap(Map("foo" -> "bar"))
+  }
+
+  def show(any: Any) = {
+    this.render(any)
   }
 }
+
+case class Widget(name: String, price: Int)
 
