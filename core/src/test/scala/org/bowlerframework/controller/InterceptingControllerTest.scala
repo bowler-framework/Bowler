efcef730e83a4b3808a4637a44fc62d07b513093
Wille Faler <wille.faler@gmail.com>
2011-02-16 01:09:18 +0000

    better tests for nested interception + ModelValidatorBuilder

--- core/src/test/scala/org/bowlerframework/controller/InterceptingControllerTest.scala
+++ core/src/test/scala/org/bowlerframework/controller/InterceptingControllerTest.scala
@@ -27,6 +27,26 @@ class InterceptingControllerTest extends ScalatraFunSuite{
 
   }
 
+  test("double intercept"){
+    val controller = new NestedInterceptingController
+    get("/doubleIntercept") {
+      assert(controller.success)
+    }
+  }
+
+}
+
+class NestedInterceptingController extends MyInterceptingController{
+  override def around(request: Request, response: Response)(controller: (Request, Response) => Unit) = {
+    super.around(request, response){
+      (x: Request, y: Response) => {
+        push(2)
+        controller(x,y)
+        this.shouldBe = 3
+      }
+    }
+  }
+
 }
 
 
@@ -42,16 +62,22 @@ class MyInterceptingController extends InterceptingController{
 
   def around(request: Request, response: Response)(controller: (Request, Response) => Unit) = {
     try{
+      println("before")
       push(1)
       controller(request, response)
       push(3)
       success = true
+      println("after")
     }catch{
       case e: IllegalArgumentException => {success = false}
     }
 
 
   }
+
+  get("/doubleIntercept")((request, response) =>{
+    push(3)
+  })
 
   get("/interceptor")((request, response) =>{
     push(2)
---
+++ core/src/test/scala/org/bowlerframework/model/ModelValidatorBuilderTest.scala
@@ -0,0 +1,36 @@
+package org.bowlerframework.model
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wfaler
+ * Date: 16/02/2011
+ * Time: 00:38
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ModelValidatorBuilderTest extends FunSuite{
+  test("test validatorBuilder"){
+    assert(None == ModelValidatorBuilder(classOf[ValidatableBean]))
+
+    ModelValidatorBuilder.registerValidatorBuilder(classOf[ValidatableBean], new ValidatableBuilder)
+
+    assert(None != ModelValidatorBuilder(classOf[ValidatableBean]))
+
+    assert(ModelValidatorBuilder(classOf[ValidatableBean]).get.isInstanceOf[ValidatableBuilder])
+    val builder1 = ModelValidatorBuilder(classOf[ValidatableBean]).get.asInstanceOf[ValidatableBuilder]
+
+    assert(builder1.initialize(ValidatableBean("new")) != builder1.initialize(ValidatableBean("new")))
+  }
+}
+
+
+case class ValidatableBean(name: String)
+
+class ValidatableBuilder extends ModelValidatorBuilder[ValidatableBean]{
+  def initialize(bean: ValidatableBean): ModelValidator = {
+    val builder = new DefaultModelValidator(classOf[ValidatableBean])
+    return builder
+  }
+}
