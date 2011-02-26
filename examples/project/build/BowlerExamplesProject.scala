b0edd92d16b939c2014de26d7ee55f322dc21329
Wille Faler <wille.faler@gmail.com>
2011-02-26 23:15:46 +0000

    added cucumber-sbt

--- examples/project/build/BowlerExamplesProject.scala
+++ examples/project/build/BowlerExamplesProject.scala
@@ -1,10 +1,12 @@
 import sbt._
+import templemore.sbt.CucumberProject
+//import bryanjswift.selenium.SeleniumPlugin
 
 
-class BowlerExamplesProject(info: ProjectInfo) extends DefaultWebProject(info){
+class BowlerExamplesProject(info: ProjectInfo) extends DefaultWebProject(info) with CucumberProject {//with SeleniumPlugin{
   val bowlerJpa = "org.bowlerframework" % "jpa-mapper_2.8.1" % "0.2"
   val bowlerSqueryl = "org.bowlerframework" % "squeryl-mapper_2.8.1" % "0.2"
-
+  val scalatest = "org.scalatest" % "scalatest" %  "1.2" % "test"
   val hibernateEntityManager = "org.hibernate" % "hibernate-entitymanager" % "3.5.1-Final"
 
   val hsqldb = "hsqldb" % "hsqldb" % "1.8.0.7" 
@@ -22,10 +24,13 @@ class BowlerExamplesProject(info: ProjectInfo) extends DefaultWebProject(info){
 
   val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
 
+  val selenium = "org.seleniumhq.selenium" % "selenium" % "2.0a4" % "test"
+
   val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots" 
   val sonatypeNexusReleases = "Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases"
 
   val scalaToolsRepo = "Scala-Tools repo" at "http://scala-tools.org/repo-releases/"
 
-val jbossRepo = "JBoss repo" at "http://repository.jboss.com/maven2/"
+  val jbossRepo = "JBoss repo" at "http://repository.jboss.com/maven2/"
+  //val openQaRepo = "Open QA repo" at "http://nexus.openqa.org/content/repositories/releases/"
 }
