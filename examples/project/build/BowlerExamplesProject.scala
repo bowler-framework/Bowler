de655de3f450f21b39e85fed96d89a60ceb1b19d
Wille Faler <wille.faler@gmail.com>
2011-03-03 23:20:53 +0000

    added testing project for managing testing dependencies (including Selenium)

--- examples/project/build/BowlerExamplesProject.scala
+++ examples/project/build/BowlerExamplesProject.scala
@@ -1,3 +1,4 @@
+
 import sbt._
 import templemore.sbt.CucumberProject
 //import bryanjswift.selenium.SeleniumPlugin
@@ -6,7 +7,7 @@ import templemore.sbt.CucumberProject
 class BowlerExamplesProject(info: ProjectInfo) extends DefaultWebProject(info) with CucumberProject {//with SeleniumPlugin{
   val bowlerJpa = "org.bowlerframework" % "jpa-mapper_2.8.1" % "0.2.2-SNAPSHOT"
   val bowlerSqueryl = "org.bowlerframework" % "squeryl-mapper_2.8.1" % "0.2.2-SNAPSHOT"
-  val scalatest = "org.scalatest" % "scalatest" %  "1.2" % "test"
+ // val scalatest = "org.scalatest" % "scalatest" %  "1.2" % "test"
   val hibernateEntityManager = "org.hibernate" % "hibernate-entitymanager" % "3.5.1-Final"
 
   val hsqldb = "hsqldb" % "hsqldb" % "1.8.0.7" 
@@ -19,12 +20,14 @@ class BowlerExamplesProject(info: ProjectInfo) extends DefaultWebProject(info) w
 	
   val sfl4jnop = "org.slf4j" % "slf4j-nop" % slf4jVersion % "runtime"
 
-  val jetty6 = "org.eclipse.jetty" % "jetty-server" % "7.2.0.v20101020" % "test"
+ // val jetty6 = "org.eclipse.jetty" % "jetty-server" % "7.2.0.v20101020" % "test"
 	val jettyWebapp = "org.eclipse.jetty" % "jetty-webapp" % "7.2.0.v20101020" % "test"
 
   val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
 
-  val selenium = "org.seleniumhq.selenium" % "selenium" % "2.0a4" % "test"
+  val bowlerTesting = "org.bowlerframework" % "testing_2.8.1" % "0.2.2-SNAPSHOT" % "test"
+
+ // val selenium = "org.seleniumhq.selenium" % "selenium" % "2.0a4" % "test"
 
   val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots" 
   val sonatypeNexusReleases = "Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases"
@@ -33,4 +36,9 @@ class BowlerExamplesProject(info: ProjectInfo) extends DefaultWebProject(info) w
 
   val jbossRepo = "JBoss repo" at "http://repository.jboss.com/maven2/"
   //val openQaRepo = "Open QA repo" at "http://nexus.openqa.org/content/repositories/releases/"
+
+
+ /* override def afterCucumberSuite = {
+    CucumberSuiteUtils.stop
+  } */
 }
---
+++ examples/src/test/scala/features/CucumberSuiteUtils.scala
@@ -0,0 +1,11 @@
+package features
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wfaler
+ * Date: 03/03/2011
+ * Time: 23:03
+ * To change this template use File | Settings | File Templates.
+ */
+
+object CucumberSuiteUtils 
