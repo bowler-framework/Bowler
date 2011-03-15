e249b4ebc18895d1d63703525ce5d15061601159
Wille Faler <wille.faler@gmail.com>
2011-03-15 21:21:25 +0000

    cleaned up Component

--- core/src/main/scala/org/bowlerframework/view/squery/Component.scala
+++ core/src/main/scala/org/bowlerframework/view/squery/Component.scala
@@ -15,11 +15,11 @@ import java.util.concurrent.ConcurrentHashMap
  * To change this template use File | Settings | File Templates.
  */
 
-trait Component{
+trait Component {
   def render: NodeSeq = Component.getTemplate(this.getClass, Component.localisationPreferences)
 }
 
-object Component{
+object Component {
   val templateCache = new ConcurrentHashMap[Class[_], HashMap[String, Option[NodeSeq]]]
   val templateResolver = new ClasspathTemplateResolver
   val types = List(".html", ".xhtml", ".xml")
@@ -27,48 +27,56 @@ object Component{
   private def uri(cls: Class[_]): String = "/" + cls.getName.replace(".", "/")
 
   def localisationPreferences: List[String] = {
-    if(RequestScope.request != null && RequestScope.request.getLocales != null)
+    if (RequestScope.request != null && RequestScope.request.getLocales != null)
       return RequestScope.request.getLocales
     else return Nil
   }
 
 
   private def getTemplate(cls: Class[_], locales: List[String]): NodeSeq = {
-    if(templateCache.get(cls) == null)
+    if (templateCache.get(cls) == null)
       templateCache.put(cls, new HashMap[String, Option[NodeSeq]])
     val map = templateCache.get(cls)
-    try{
-      if(locales == Nil){
+    try {
+      if (locales == Nil) {
         val option = map("default")
-        if(option == None)
+        if (option == None)
           return getTemplate(cls.getSuperclass, localisationPreferences)
         else return option.get
-      }else{
+      } else {
         val option = map(locales.head)
-        if(option == None){
+        if (option == None) {
           val newLocales = locales.drop(1)
           return getTemplate(cls, newLocales)
-        }else return option.get
+        } else return option.get
       }
-    }catch{
-      case e: NoSuchElementException => {}// do nothing
-    }
-    try{
-      val nodeSeq = XML.load(new StringReader(templateResolver.resolveResource(uri(cls), Component.types, {if(locales != Nil) List(locales.head); else Nil}).template)).asInstanceOf[NodeSeq]
-      map.put({if(locales != Nil) locales.head; else "default"}, Some(nodeSeq))
-      return nodeSeq
-    }catch{
-      case e: IOException => {
-        map.put({if(locales != Nil) locales.head; else "default"}, None)
-        if(locales != Nil){
-          val newLocales = locales.drop(1)
-          getTemplate(cls, newLocales)
-        }else if(cls.getSuperclass != null && classOf[Component].isAssignableFrom(cls.getSuperclass) && locales == Nil){
-          getTemplate(cls.getSuperclass, localisationPreferences)
+    } catch {
+      case e: NoSuchElementException => {
+        try {
+          val nodeSeq = XML.load(new StringReader(templateResolver.resolveResource(uri(cls), Component.types, {
+            if (locales != Nil) List(locales.head); else Nil
+          }).template)).asInstanceOf[NodeSeq]
+          map.put({
+            if (locales != Nil) locales.head; else "default"
+          }, Some(nodeSeq))
+          return nodeSeq
+        } catch {
+          case e: IOException => {
+            map.put({
+              if (locales != Nil) locales.head; else "default"
+            }, None)
+            if (locales != Nil) {
+              val newLocales = locales.drop(1)
+              getTemplate(cls, newLocales)
+            } else if (cls.getSuperclass != null && classOf[Component].isAssignableFrom(cls.getSuperclass) && locales == Nil) {
+              getTemplate(cls.getSuperclass, localisationPreferences)
+            }
+            else
+              throw new IOException("Can't find any markup for Component of type " + cls.getName)
+          }
         }
-        else
-          throw new IOException("Can't find any markup for Component of type " + cls.getName)
       }
     }
+
   }
 }
