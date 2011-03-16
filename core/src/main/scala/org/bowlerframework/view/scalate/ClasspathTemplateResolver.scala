7ec2025d12737d0f4c7b5367399c865ebd9a85f3
Wille Faler <wille.faler@gmail.com>
2011-03-16 00:00:01 +0000

    added template caching

--- core/src/main/scala/org/bowlerframework/view/scalate/ClasspathTemplateResolver.scala
+++ core/src/main/scala/org/bowlerframework/view/scalate/ClasspathTemplateResolver.scala
@@ -1,7 +1,8 @@
 package org.bowlerframework.view.scalate
 
 import com.recursivity.commons.{StringInputStreamReader, ClasspathResourceResolver}
-import java.io.{InputStream}
+import collection.mutable.HashMap
+import java.io.{IOException, InputStream}
 
 
 class ClasspathTemplateResolver extends TemplateResolver with StringInputStreamReader {
@@ -10,20 +11,35 @@ class ClasspathTemplateResolver extends TemplateResolver with StringInputStreamR
    * Gets a resource with no fallback to default if localised file does not exist
    */
   def getAbsoluteResource(path: String, fileType: String, locale: String = null): Template = {
-    var is: InputStream = null
-    try {
-      var realPath: String = null
+    var realPath: String = null
       if (locale == null)
         realPath = path + fileType
       else
         realPath = path + "_" + locale + fileType
-      val obj = new ClasspathResourceResolver
-      is = obj.getClass.getResourceAsStream(realPath)
-      val templateString = this.load(is)
-      return Template(realPath, templateString)
-    } finally {
-      is.close
-    }
+      try{
+        val template = ClasspathTemplateResolver.templates(realPath)
+        return template.getOrElse(throw new IOException("No Template with path " + realPath))
+      }catch{
+        case e: NoSuchElementException => {
+          var is: InputStream = null
+          try {
+            val obj = new ClasspathResourceResolver
+            is = obj.getClass.getResourceAsStream(realPath)
+            val templateString = this.load(is)
+            val tmpl = Template(realPath, templateString)
+            if(fileType != ".html" && fileType != ".xml" && fileType != ".xhtml")
+              ClasspathTemplateResolver.templates.put(realPath, Some(tmpl))
+            return tmpl
+          }catch{
+            case ie: IOException => {
+              ClasspathTemplateResolver.templates.put(realPath, None)
+              throw ie
+            }
+          }finally {
+            is.close
+          }
+        }
+      }
   }
 
   def getAbsoluteResource(uri: String): String = {
@@ -37,4 +53,8 @@ class ClasspathTemplateResolver extends TemplateResolver with StringInputStreamR
     }
   }
 
+}
+
+object ClasspathTemplateResolver{
+  private val templates = new HashMap[String, Option[Template]]
 }
