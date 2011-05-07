647f1475ebcac118acd2580045aef04f9ca7126a
Wille Faler <wille.faler@gmail.com>
2011-05-07 01:00:58 +0100

    added trait for POSO routing

---
+++ core/src/main/scala/org/bowlerframework/controller/FunctionNameConventionRoutes.scala
@@ -0,0 +1,15 @@
+package org.bowlerframework.controller
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wfaler
+ * Date: 07/05/2011
+ * Time: 00:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait FunctionNameConventionRoutes{ this: Controller => {
+
+}
+POSORouteMapper(this, this)
+}
