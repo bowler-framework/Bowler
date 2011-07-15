ca0cb1e93c602ec99306d34ae6a836fa0e7085dd
Noel Kennedy <nkennedy@rvc.ac.uk>
2011-07-15 14:46:03 +0100

    added jsonp to request pipeline and added renderer

---
+++ core/src/main/scala/org/bowlerframework/view/JsonpViewRenderer.scala
@@ -0,0 +1,13 @@
+package org.bowlerframework.view
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: noel
+ * Date: 15/07/11
+ * Time: 13:16
+ * To change this template use File | Settings | File Templates.
+ */
+
+class JsonpViewRenderer {
+
+}
